81c805525ed26ebe6017c7a982aa8467d77c51c7
amirLms <amir.rma69@gmail.com>
2020-12-22 21:48:41 +0330

    setting up...

--- app.js
+++ app.js
@@ -5,20 +5,20 @@ const transporter = nodemailer.createTransport({
      service: 'gmail',
      auth: {
          xoauth2: xoauth2.createXOAuth2Generator({
-             user: 'amir.rma69@gmail.com',
-             pass:'Gb696969+',
-             clientId: '838971535835-14khqk8ppqlb4icnd0u86oa6dme6hngd.apps.googleusercontent.com',
-             clientSecret: 'YN_eIpAht-mTDd4PGlVrgDnw',
-             refreshToken:'1//04zdcCGtNi5jaCgYIARAAGAQSNwF-L9IrIHVdoczda0zEtg2up4HQfH-AMZruSq6Y8Np5Kj3slqhfFu5R7iw3g9ReALlxOZf2xEU'
+             user: '<your email>',
+             pass:'<your password>',
+             clientId: 'clientId you recieve from google API console',
+             clientSecret: 'client sercret you recieve from google API console',
+             refreshToken:'refresh token you recieve from google api playground'
          })
      }
  })
 
 const mailOptions = {
-    from: 'amir.rma69@gmail.com',
-    to:'amirajpc69@gmail.com',
-    subject:'test',
-    text:'hi amoqli'
+    from: 'sender email',
+    to:'reciever email',
+    subject:'subject of mail',
+    text:'text of mail'
 }
 
 transporter.sendMail(mailOptions , function(err,res){
